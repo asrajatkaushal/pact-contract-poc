cf5c3a080993de54ca64c77de9fb2dc683bcb1c2
Rajat Kaushal <asrajatkaushal@gmail.com>
2020-08-20 03:06:25 +0400

    removed extra imports

--- pact-producer/src/test/kotlin/com/example/pactproducer/GetTransactionsContractVerificationTest.kt
+++ pact-producer/src/test/kotlin/com/example/pactproducer/GetTransactionsContractVerificationTest.kt
@@ -3,14 +3,11 @@ package com.example.pactproducer
 import au.com.dius.pact.provider.junit.Consumer
 import au.com.dius.pact.provider.junit.Provider
 import au.com.dius.pact.provider.junit.loader.PactBroker
-import au.com.dius.pact.provider.junit5.HttpTestTarget
 import au.com.dius.pact.provider.junit5.PactVerificationContext
 import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.TestTemplate
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.boot.web.server.LocalServerPort
 
 
 @Consumer("transactionsConsumer")
